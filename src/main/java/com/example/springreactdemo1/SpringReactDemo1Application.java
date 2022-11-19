92f1681c384993e738bed4e903d1246e23d1876c
rahulkumar <rahularya0083@gmail.com>
2022-11-19 11:28:38 +0530

    reactive springboot first commit

---
+++ src/main/java/com/example/springreactdemo1/SpringReactDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.springreactdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactDemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springreactdemo1/SpringReactDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreactdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
